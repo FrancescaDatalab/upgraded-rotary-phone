61d224291739b4ec5ac319e06a011db7d137bc0a
FrancescaDatalab <francesca@datalab.co.za>
2025-12-04 19:50:42 +0200

    Adjust for search_on

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,11 @@ name_on_order = st.text_input('Name on smoothie')
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
+pd_df = my_dataframe.to_pandas()
+
+st.dataframe(pd_df)
+# st.stop()
+
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients",
     my_dataframe,
@@ -28,6 +33,9 @@ if ingredients_list:
     # ingredients_string = ''
 
     for fruit_chosen in ingredients_list:
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
         st.subheader(f"Information about {fruit_chosen}")
 
         smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
