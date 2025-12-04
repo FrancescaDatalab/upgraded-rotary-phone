3606a43b48c7062570d58cd18a9bb232b5a06c6c
FrancescaDatalab <francesca@datalab.co.za>
2025-12-04 19:48:44 +0200

    Add pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -12,7 +13,7 @@ session = cnx.session()
 
 name_on_order = st.text_input('Name on smoothie')
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients",
