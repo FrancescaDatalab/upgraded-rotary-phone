3591ac31b8551611a950add50723477638ed1a06
FrancescaDatalab <francesca@datalab.co.za>
2025-12-04 19:12:24 +0200

    Correct session

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.title(f":cup_with_straw: Customize Your Smoothie!:cup_with_straw:")
 st.write('Choose the fruits you want in your smoothie.')
 
 cnx = st.connection("snowflake")
-session = get_active_session()
+session = cnx.session()
 
 name_on_order = st.text_input('Name on smoothie')
 
